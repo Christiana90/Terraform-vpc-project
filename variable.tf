d12397ad4edd676fa593593b97372df3a0db98ae
christiana90 <chinweonyebueke9@gmail.com>
2024-10-11 02:28:11 +0200

    creation of VPC, Subnet, route table and IG

--- variable.tf
+++ variable.tf
@@ -6,6 +6,41 @@ variable "vpc-cidr_block" {
 
 variable "public-subnet-1-cidr_block" {
   default = "10.0.0.0/24"
-  description = "Public subnet 1CIDR Block"
+  description = "Public subnet 1 CIDR Block"
+  type = string
+}
+
+
+variable "public-subnet-2-cidr_block" {
+  default = "10.0.1.0/24"
+  description = "Public subnet 2 CIDR Block"
+  type = string
+}
+
+
+variable "private-subnet-1-cidr_block" {
+  default = "10.0.2.0/24"
+  description = "private subnet 1 CIDR Block"
+  type = string
+}
+
+
+variable "private-subnet-2-cidr_block" {
+  default = "10.0.3.0/24"
+  description = "private subnet 2 CIDR Block"
+  type = string
+}
+
+
+variable "private-subnet-3-cidr_block" {
+  default = "10.0.4.0/24"
+  description = "private subnet 3 CIDR Block"
+  type = string
+}
+
+
+variable "private-subnet-4-cidr_block" {
+  default = "10.0.5.0/24"
+  description = "private subnet 4 CIDR Block"
   type = string
 }
